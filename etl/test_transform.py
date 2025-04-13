29d62b2dc6a55015d66b7d2eb0481cc4ff254c44
Simeon Adesanya <simeon.j.a@outlook.com>
2025-04-13 21:23:15 +0100

    remove extra lines and rename test functions

--- etl/test_transform.py
+++ etl/test_transform.py
@@ -10,93 +10,95 @@ def test_to_snake_case_base_case_1():
     """Tests base case for to_snake_case"""
 
     result = to_snake_case("camelCase")
-
     assert result == "camel_case"
 
 def test_to_snake_case_base_case_2():
     """Tests base case for to_snake_case"""
 
     result = to_snake_case("camelCasecamelCase")
-
     assert result == "camel_casecamel_case"
 
 def test_to_snake_case_base_case_3():
     """Tests base case for to_snake_case"""
 
     result = to_snake_case("snakecase")
-
     assert result == "snakecase"
 
 def test_to_snake_case_base_case_4():
     """Tests base case for to_snake_case"""
 
     result = to_snake_case("snakeCase")
-
     assert result == "snake_case"
 
 def test_to_snake_case_base_case_5():
     """Tests base case for to_snake_case"""
 
     result = to_snake_case("snake1Case")
-
     assert result == "snake1_case"
 
 def test_to_snake_case_base_case_6():
     """Tests base case for to_snake_case"""
 
     result = to_snake_case("snake1 Case")
-
     assert result == "snake1_case"
 
-def test_to_snake_case_base_case_7():
-    """Tests base case for to_snake_case"""
+def test_to_snake_case_leading_spaces():
+    """Tests to_snake_case with leading spaces"""
 
-    result = to_snake_case("UpperCamelCase")
-
+    result = to_snake_case("     upperCamelCase")
     assert result == "upper_camel_case"
 
-def test_to_snake_case_base_case_8():
-    """Tests base case for to_snake_case"""
+def test_to_snake_case_normal_spaces():
+    """Tests to_snake_case with normal spacing"""
 
     result = to_snake_case("Upper Camel Case")
-
     assert result == "upper_camel_case"
 
-def test_to_snake_case_base_case_9():
-    """Tests base case for to_snake_case"""
+def test_to_snake_case_trailing_spaces():
+    """Tests to_snake_case with trailing spaces"""
 
-    result = to_snake_case("starPowers")
-
+    result = to_snake_case("starPowers     ")
     assert result == "star_powers"
 
-def test_to_snake_case_edge_case_1():
-    """Tests esge case for to_snake_case"""
+def test_to_snake_case_value_error_raised_with_empty_string():
+    """Tests value error is raised for to_snake_case
+     having an input of an empty string"""
 
-    with pytest.raises(Exception):
+    with pytest.raises(ValueError):
         to_snake_case("")
 
-def test_to_snake_case_edge_case_2():
-    """Tests esge case for to_snake_case"""
+def test_to_snake_case_value_error_raised_with_whitespace():
+    """Tests value error is raised for to_snake_case
+    when input is whitespace"""
 
-    with pytest.raises(Exception):
+    with pytest.raises(ValueError):
         to_snake_case(" ")
 
-def test_to_snake_case_edge_case_3():
-    """Tests esge case for to_snake_case"""
+def test_to_snake_case_type_error_with_wrong_input():
+    """Tests type input is raised for to_snake_case
+    with an input that is not a string"""
 
-    with pytest.raises(Exception):
+    with pytest.raises(TypeError):
         to_snake_case(1)
 
-def test_to_snake_case_edge_case_4():
-    """Tests esge case for to_snake_case"""
+def test_to_snake_case_5v5():
+    """Tests case for to_snake_case 5v5 game mode"""
 
     result = to_snake_case("brawlBall5V5")
     assert result == "brawl_ball_5v5"
 
-def test_to_snake_case_edge_case_5():
-    """Tests esge case for to_snake_case"""
+def test_to_snake_case_5v5_trailing_space():
+    """Tests case for to_snake_case with 5v5 game mode
+    and trailing spaces"""
 
     result = to_snake_case("brawlBall5V5   ")
+    assert result == "brawl_ball_5v5"
+
+def test_to_snake_case_5v5_leading_space():
+    """Tests case for to_snake_case with 5v5 game mode
+    and leading spaces"""
+
+    result = to_snake_case("    brawlBall5V5")
     assert result == "brawl_ball_5v5"
 
 def test_brawler_name_value_to_title_base_case_1():
