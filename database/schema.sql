2d8aa91e1af306256717c527df4bd6c380ed4e8a
Simeon Adesanya <simeon.j.a@outlook.com>
2025-04-22 09:38:38 +0100

    Add battle type table to schema

--- database/schema.sql
+++ database/schema.sql
@@ -104,12 +104,21 @@ CREATE TABLE bs_event (
   PRIMARY KEY (bs_event_id)
 );
 
+DROP TABLE IF EXISTS battle_type CASCADE;
+CREATE TABLE battle_type (
+  battle_type_id SMALLINT GENERATED ALWAYS AS IDENTITY,
+  battle_type_name TEXT NOT NULL,
+  created_at TIMESTAMPTZ DEFAULT NOW(),
+  PRIMARY KEY (battle_type_id)
+); 
+
 DROP TABLE IF EXISTS battle CASCADE;
 CREATE TABLE battle (
   id INT GENERATED ALWAYS AS IDENTITY,
   player_tag VARCHAR(50),
   battle_time TIMESTAMPTZ,
   bs_event_id INT NOT NULL,
+  battle_type_id SMALLINT NOT NULL,
   result TEXT NOT NULL,
   duration SMALLINT NOT NULL,
   trophy_change SMALLINT, 
@@ -117,5 +126,7 @@ CREATE TABLE battle (
   star_player boolean,
   PRIMARY KEY (id),
   FOREIGN KEY (bs_event_id) REFERENCES bs_event (bs_event_id),
-  FOREIGN KEY (player_tag) REFERENCES player (player_tag)
+  FOREIGN KEY (player_tag) REFERENCES player (player_tag),
+  FOREIGN KEY (battle_type_id) REFERENCES battle_type (battle_type_id)
 );
+
